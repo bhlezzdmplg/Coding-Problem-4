5e59ba2459b2a1d3d155eb8d0755b9ed512e0e4c
bhlezzdmplg <yharrasallyguiao@gmail.com>
2024-11-08 21:36:11 +0800

    Update favNumber.js

--- favNumber.js
+++ favNumber.js
@@ -1,14 +1,33 @@
-let favNumber = 24;
+const readline = require('readline');
 
-let guess = parseInt(prompt("Guess my favorite number: "));
+// Step 1: Set up the readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-while (guess !== favNumber) {
-  if (guess > favNumber) {
-    alert("Too high!");
-  } else {
-    alert("Too low!");
-  }
-  guess = parseInt(prompt("Guess again: "));
+// Step 2: Declare the favorite number
+const theFavNumber = 42;
+
+// Function to prompt the user and handle the guess
+function guessNumber() {
+  rl.question("Guess the favorite number: ", (input) => {
+    const guess = parseInt(input);
+
+    if (isNaN(guess)) {
+      console.log("Please enter a valid number.");
+    } else if (guess > theFavNumber) {
+      console.log("Too high!");
+      guessNumber();
+    } else if (guess < theFavNumber) {
+      console.log("Too low!");
+      guessNumber();
+    } else {
+      console.log("Correct! You guessed the favorite number.");
+      rl.close(); // Close the interface when the guess is correct
+    }
+  });
 }
 
-alert("Correct!");
+// Start the game
+guessNumber();
